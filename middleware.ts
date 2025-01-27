f39c37c83101f265d03070b4f5b2673cc0f1b456
Navdeep Singh <[singh.nav4444@gmail.com]>
2025-01-27 22:03:30 +0200

    add new middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
+// const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
 
-// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
